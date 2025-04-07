78b994915d1e43197bda6b61a02aa48803d9db84
greyog <greyog@gmail.com>
2025-04-08 01:14:41 +0300

    eth_usdt_cumulative_stop_grid

--- src/main/java/com/bybit/api/client/domain/market/response/instrumentInfo/LotSizeFilter.java
+++ src/main/java/com/bybit/api/client/domain/market/response/instrumentInfo/LotSizeFilter.java
@@ -1,9 +1,11 @@
 package com.bybit.api.client.domain.market.response.instrumentInfo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 import java.math.BigDecimal;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 public class LotSizeFilter {
     private BigDecimal maxOrderQty;
---
+++ src/main/java/eth_usdt_cumulative_stop_grid/Handler.java
@@ -0,0 +1,27 @@
+package eth_usdt_cumulative_stop_grid;
+
+import java.util.Map;
+import java.util.function.Function;
+
+class Request {
+    private String httpMethod;
+    private Map<String, String> headers;
+}
+
+class Response {
+    private int statusCode;
+    private String body;
+
+    public Response(int statusCode, String body) {
+        this.statusCode = statusCode;
+        this.body = body;
+    }
+}
+
+public class Handler implements Function<Request, Response> {
+    @Override
+    public Response apply(Request request) {
+        Main.main(new String[0]);
+        return new Response(200, "Hello, thanks i'm fine!");
+    }
+}
